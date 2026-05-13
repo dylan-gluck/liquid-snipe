d29a06969259561b7202b7e054f2d405aa810073
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2026-05-13 16:11:09 -0400

    chore: removes v1 implementation, keeps script to demo events

--- src/blockchain/index.ts
+++
@@ -1,4 +0,0 @@
-export { ConnectionManager, ConnectionStatus, ConnectionMetrics } from './connection-manager';
-export { SolanaUtils, AccountData, TokenAccountInfo, TransactionResult } from './solana-utils';
-export { BlockchainWatcher, WatcherStatus, PoolInfo } from './blockchain-watcher';
-export { TokenInfoService, TokenInfo, TokenHolder, RiskAssessment } from './token-info-service';
--- src/core/workflows/index.ts
+++
@@ -1,5 +0,0 @@
-export { TradingWorkflowCoordinator } from './trading-workflow';
-export { PositionWorkflowCoordinator } from './position-workflow';
-export { UserInteractionWorkflowCoordinator } from './user-interaction-workflow';
-export { DataManagementWorkflowCoordinator } from './data-management-workflow';
-export { ErrorRecoveryWorkflowCoordinator } from './error-recovery-workflow';
